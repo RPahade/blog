24cfb35a7ed70cf98752da86422bbac26ba380e9
RPahade <rohpahade2@gmail.com>
2025-04-04 21:54:24 +0530

    final changes

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+const url = 'https://blog-app-x2a6.onrender.com';
 
 
 let gfs, gridfsBucket;
