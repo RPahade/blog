064364195dbdba6b2c253e3c6a67c9a8b1ec8008
RPahade <101712261+RPahade@users.noreply.github.com>
2025-04-04 22:55:54 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = 'https://blog-app-x2a6.onrender.com';
+const API_URL = 'https://blogapp-46cj.onrender.com';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
